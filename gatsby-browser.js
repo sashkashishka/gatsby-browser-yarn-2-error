1e48cd77f54fe500305c2c2e5b2c2db1b418231e
Aleksandr Taperik <alexandr1696@hotmail.com>
2020-05-01 18:20:51 +0300

    bug reproduction

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,20 @@
  */
 
 // You can delete this file if you're not using it
+
+// with this syntax leads to develop build failure
+export const onClientEntry = async () => {
+  if (typeof IntersectionObserver === `undefined`) {
+    await import(`intersection-observer`);
+  }
+}
+
+// when using this syntax - everything is okay
+// export const onClientEntry = () => {
+//  if (typeof IntersectionObserver !== 'undefined') {
+//    return import('intersection-observer');
+//  }
+// 
+//  return Promise.resolve();
+// } 
+
